df0ac146ae8fef9ec5e7230b9b4285ad8da3f675
Olivier Sputael <sputael.o@gmail.com>
2022-11-04 16:17:55 +0100

    Fixed small display errors

--- components/beerDetails.tsx
+++ components/beerDetails.tsx
@@ -20,26 +20,6 @@ const BeerDetails = ({ beer, close }: BeerBetailsProps) => {
                 <p>Retour à la carte</p>
             </div>
             <BeerLayout beer={beer} imgSrc={beer.image} imgW={71} imgH={289} />
-            {/*}
-            <div className={css.infoWrapper}>
-                <div className={css.beerInfoImage}>
-                    <Image src='/beer_cropped.png' width='71' height='289' />
-                </div>
-                <div className={css.beerInfoContent}>
-                    <h1>{beer.name}</h1>
-                    <p className={css.detailType}>{beer.type}</p>
-                    <div className='flex mt-4'>
-                        <div className={css.dataContainer}>{`${beer.degree} %`}</div>
-                        <div className={css.dataContainer}>{`IBU ${beer.ibu}`}</div>
-                    </div>
-                    <div className='flex w-full mt-9 pb-3 border-b border-b-dark-white'>
-                        <i className='mr-3'>{breweryIcon}</i>
-                        <p className='text-dark-green'>{beer.brewery}</p>
-                    </div>
-                    <p className='my-6 font-light'>{beer.description}</p>
-                </div>
-            </div>
-    */}
         </div>
     );
 };
--- components/beerLayout.tsx
+++ components/beerLayout.tsx
@@ -5,6 +5,8 @@ import { Beers } from '@lib/types';
 
 import { breweryIcon } from './icons';
 import css from '@styles/beers.module.css';
+import useMediaQuery from './useMediaQuery';
+import { MAX_WIDTH_MOB } from '@lib/constants';
 
 type BeerLayoutProps = {
     beer: Beers;
@@ -15,27 +17,64 @@ type BeerLayoutProps = {
 
 const BeerLayout: React.FunctionComponent<BeerLayoutProps> = (props) => {
     const { beer, imgSrc, imgW, imgH } = props;
-    console.log('desc: ', beer.description);
-    return (
-        <div className={css.infoWrapper}>
+    const isMobile = useMediaQuery(MAX_WIDTH_MOB);
+
+    const beerImgJsx = () => {
+        return (
             <div className={css.beerInfoImage}>
                 <Image src={process.env.NEXT_PUBLIC_IMG_CDN + imgSrc + '/'} width={imgW} height={imgH} />
             </div>
-            <div className={css.beerInfoContent}>
-                <h1>{beer.name}</h1>
-                <p className={css.detailType}>{beer.type}</p>
-                <div className='flex mt-4'>
-                    <div className={css.dataContainer}>{`${beer.degree} %`}</div>
-                    <div className={css.dataContainer}>{`IBU ${beer.ibu}`}</div>
+        );
+    };
+    const beerInfoJsx = () => {
+        let jsx;
+
+        if (isMobile) {
+            jsx = (
+                <div className='flex flex-col'>
+                    <div className='flex justify-evenly'>
+                        {beerImgJsx()}
+                        <div className={css.beerInfoContent}>
+                            <h1>{beer.name}</h1>
+                            <p className={css.detailType}>{beer.type}</p>
+                            <div className='flex mt-4'>
+                                <div className={css.dataContainer}>{`${beer.degree} %`}</div>
+                                <div className={css.dataContainer}>{`IBU ${beer.ibu}`}</div>
+                            </div>
+                            <div className='flex w-full mt-9 pb-3 border-b border-b-dark-white'>
+                                <i className='mr-3'>{breweryIcon}</i>
+                                <p className='text-dark-green'>{beer.brewery}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className='mb-6 font-light whitespace-pre-line'>{beer.description}</p>
                 </div>
-                <div className='flex w-full mt-9 pb-3 border-b border-b-dark-white'>
-                    <i className='mr-3'>{breweryIcon}</i>
-                    <p className='text-dark-green'>{beer.brewery}</p>
-                </div>
-                <p className='my-6 font-light whitespace-pre-line'>{beer.description}</p>
-            </div>
-        </div>
-    );
+            );
+        } else {
+            jsx = (
+                <>
+                    {beerImgJsx()}
+                    <div className={css.beerInfoContent}>
+                        <h1>{beer.name}</h1>
+                        <p className={css.detailType}>{beer.type}</p>
+                        <div className='flex mt-4'>
+                            <div className={css.dataContainer}>{`${beer.degree} %`}</div>
+                            <div className={css.dataContainer}>{`IBU ${beer.ibu}`}</div>
+                        </div>
+                        <div className='flex w-full mt-9 pb-3 border-b border-b-dark-white'>
+                            <i className='mr-3'>{breweryIcon}</i>
+                            <p className='text-dark-green'>{beer.brewery}</p>
+                        </div>
+                        <p className='my-6 font-light whitespace-pre-line'>{beer.description}</p>
+                    </div>
+                </>
+            );
+        }
+
+        return jsx;
+    };
+
+    return <div className={css.infoWrapper}>{beerInfoJsx()}</div>;
 };
 
 export default BeerLayout;
